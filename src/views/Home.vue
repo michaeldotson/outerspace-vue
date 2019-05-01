69c9ae262a0bc9c45fcfde3eff8f7d32a01fa90d
mike <michaeldotson@protonmail.com>
2019-04-30 17:59:46 -0700

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to our Solar System</h1>
+    <!-- Loop through Systems -->
+    <div class="planets" v-for="system in systems">
+      <ul>
+        <li>
+          <h2>System Name: {{system.name}}</h2>
+          <h4>System Radius: {{system.radius}}</h4>
+        </li>
+      </ul>
+      
+
+      <!-- Loop through ships -->
+      <div v-for="ship in system.ships">
+        <ul>
+          <li>
+            <hr>
+            <h3>Ship Name: {{ship.name}}</h3>
+            <h3>Velocity: {{ship.velocity}}</h3>
+            <hr>
+         </li>
+        </ul>
+      </div>
+      
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      systems: [],
+      system: {},
+      ships:[],
+      ship: {},
+    };
+  },
+  created: function() {
+    axios.get("/api/ships").then(response => {
+      console.log(response.data);
+      this.ships = response.data;
+    });
+    axios.get("/api/systems").then(response => {
+      console.log(response.data);
+      this.systems = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
